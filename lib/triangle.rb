84a9798a0f0461101a6af97f74d76687f7bc299f
Marvin Reyes <reyemarv@gmail.com>
2020-03-07 05:21:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,10 @@ class Triangle
 
   attr_accessor :length1, :length2, :length3 
   
-  def intitialize(attrs)
-    @length1
-    
+  def intitialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
   end 
   
   class TriangleError < StandardError
