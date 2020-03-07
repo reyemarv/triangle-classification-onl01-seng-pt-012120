7312635a3542396bdb00f59588b24f6f619fa81f
Marvin Reyes <reyemarv@gmail.com>
2020-03-07 05:55:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,10 +15,11 @@ class Triangle
     if @length1 == @length2 && @length2 == @length3 && @length1 == @length3
       :equilateral
     elsif 
-      @length1 == @length3 && @length1 != @length3 && @length2 == @length3
+      @length1 == @length2 || @length2 == @length3 || @length1 == @length3
       :isosceles 
     else 
-
+      @length1 != @length2 && @length2 != @length3 && @length1 != @length3
+      :scalene
     end 
   end 
   end 
